0ae6bcfabb23d16bb575357843f01eda1c6925f1
leesh235 <leeshgh235@gmail.com>
2022-07-12 18:30:44 +0900

    fix bug

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -6,7 +6,7 @@ const Profile = () => {
     const history = useNavigate();
     const onLogOutClick = () => {
         authService.signOut();
-        history.push("/");
+        history("/");
     };
     return (
         <>
